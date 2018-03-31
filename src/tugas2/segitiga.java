3471957105fa0b8d6e59f751ab94e04f23264d50
Hindun <32257792+Hindun@users.noreply.github.com>
2018-03-31 00:28:19 -0700

    Membuat atribut dan  behavior

--- src/tugas2/segitiga.java
+++ src/tugas2/segitiga.java
@@ -10,5 +10,14 @@ package tugas2;
  * @author Ndunh
  */
 public class segitiga {
-    
+ int sisi1;
+ int sisi2;
+ int sisi3;
+ 
+ void cetakinfo (){
+     System.out.println("sisi1 : "+sisi1);
+     System.out.println("sisi2 : "+sisi2);
+     System.out.println("sisi3 : "+sisi3);
+     
+ }
 }
